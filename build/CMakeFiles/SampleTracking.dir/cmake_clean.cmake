1012e0416a54edf7929e7f689e544c60b8f066dc
Bailey <bailey.pearson@vanderbilt.edu>
2018-02-05 15:37:43 -0600

    Added build directory and the sample video

---
+++ build/CMakeFiles/SampleTracking.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SampleTracking.dir/SampleTracking.cpp.o"
+  "SampleTracking.pdb"
+  "SampleTracking"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SampleTracking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
